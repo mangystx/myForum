a0658a8fa3eb1d72da3e53329245f30e12770dd4
Andrey <111135827+mangystx@users.noreply.github.com>
2023-10-02 23:56:55 +0300

    startLoginSystem

--- Program.cs
+++ Program.cs
@@ -1,6 +1,119 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using myForum.Dal.Structures;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using myForum.Dal.Models;
+
+var dbContext = new MyForumDbContextFactory().CreateDbContext(args);
+const string adminRole = "admin";
+const string userRole = "user";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/accessdenied";
+    });
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
+app.MapGet("/signUp", async context =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync(File.ReadAllText("wwwroot/html/signUp.html"));
+});
+
+app.MapPost("/signUp", async (HttpContext context) =>
+{
+    var form = context.Request.Form;
+    if (!form.ContainsKey("userName") || !form.ContainsKey("password"))
+    {
+        string message = String.Empty;
+        if (!form.ContainsKey("userName") && !form.ContainsKey("password"))
+        {
+            message = "User name and password are not set";
+        }
+        else if (!form.ContainsKey("userName")) message = "User name are not set";
+        else message = "Password are not set";
+
+        return Results.BadRequest(message);
+    }
+
+    string userName = form["userName"];
+    string password = form["password"];
+
+    if (dbContext.Users.Any(u => u.Name == userName)) return Results.BadRequest("User already exists");
+    User user = new User { Name = userName, Role = userRole, Password = password };
+    dbContext.Users.Add(user);
+    dbContext.SaveChanges();
+    
+    var claims = new List<Claim>
+    {
+        new (ClaimTypes.Name, userName),
+        new (ClaimTypes.Role, user.Role)
+    };
+
+    await context.SignInAsync(
+        new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme)));
+    return Results.Redirect("/");
+});
+
+app.MapGet("/login", async context =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.WriteAsync(File.ReadAllText("wwwroot/html/login.html"));
+});
+
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    var form = context.Request.Form;
+    if (!form.ContainsKey("userName") || !form.ContainsKey("password"))
+    {
+        string message = String.Empty;
+        if (!form.ContainsKey("userName") && !form.ContainsKey("password"))
+        {
+            message = "User name and password are not set";
+        }
+        else if (!form.ContainsKey("userName")) message = "User name are not set";
+        else message = "Password are not set";
+
+        return Results.BadRequest(message);
+    }
+
+    string userName = form["userName"];
+    string password = form["password"];
+    if (userName == "Admin") return Results.Redirect("/accessdenied");
+    User? user = dbContext.Users.FirstOrDefault(u => u.Name == userName && u.Password == password);
+    if (user is null) Results.Unauthorized();
+
+    var claims = new List<Claim>
+    {
+        new (ClaimTypes.Name, userName),
+        new (ClaimTypes.Role, user.Role)
+    };
+
+    await context.SignInAsync(
+        new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme)));
+    return Results.Redirect(returnUrl ?? "/");
+});
+
+app.MapGet("/accessdenied", async (HttpContext context) =>
+{
+    context.Response.StatusCode = 403;
+    await context.Response.WriteAsync("Access Denied");
+});
+
+app.MapGet("/logout", async context =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.Run();
